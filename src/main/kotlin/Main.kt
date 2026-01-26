55b53cd040f7a389d73bc0c8302718ac37540917
pomeranian88 <zevtobythompson@gmail.com>
2026-01-26 16:40:36 -0600

    Second push, added args -- 1/26, 4:40pm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
-    var myMaze = SolvableMaze("${args[0]}")
-
-    myMaze.printMaze()
-    println("ability to solve the maze: ${myMaze.solveMaze()}")
-    myMaze.printMaze()
+    if(args.size==2){
+        if(args[1]=="--solve"){
+            var myMaze = SolvableMaze("${args[0]}")
+            myMaze.printMaze()
+            println("ability to solve the maze: ${myMaze.solveMaze()}")
+            myMaze.printMaze()
+        }
+        else{println("You probably entered something wrong -- redo your entry")}
+    }
+    else if(args.size==1){
+        var myMaze = SolvableMaze("${args[0]}")
+        myMaze.printMaze()
+    }
+    else{
+        println("You probably entered something wrong -- redo your entry")
+    }
 }
